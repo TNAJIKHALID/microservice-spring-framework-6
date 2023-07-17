41560e3880d3f3aea05fe5a94399cb4bc46fdabc
Lahcen Amghar <lamghar@smb.sqli.com>
2023-07-17 12:18:46 +0100

    remove reporting service

--- transaction-service/src/main/java/com/example/transactionservice/exception/RequestNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.transactionservice.exception;
-
-public class RequestNotFoundException extends RuntimeException{
-    public RequestNotFoundException(String message) {
-        super(message);
-    }
-}
